70012e0e243ff5db6119c5e3ff1f0b00e9690aa4
pugalkmc <pugalkmc@gmail.com>
2024-06-19 09:16:48 +0530

    wsgi setup

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from main import app
 import os
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
